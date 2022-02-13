beab07855731e1279aca80e367388a94996e9af9
jonathanlao <jonathan.lao@braze.com>
2022-02-13 17:02:55 -0500

    Add latest bootstrap and fake data

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
 from flask import render_template
 from app import app
+import requests
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    url = 'https://rest.iad-01.braze.com/'
+    campaign_list_uri = '/campaigns/list'
+    API_KEY = 'c2b6c371-37f6-4d00-b925-c98c88e6d3e2'
+    headers = {"Authorization": "Bearer " + API_KEY}
+    params = {
+        'page': 0,
+        'include_archived': True,
+        'sort_direction': 'asc',
+        'last_edit.time[gt]': '1992-06-28T23:59:59-5:00'
+    }
+    res = requests.get(url + campaign_list_uri, headers=headers, params=params)
+    print(res.json())
+
+    campaigns = [
+        {'id': 1, 'name': 'Campaign 1', 'tags': ['marketing', 'promotional']},
+        {'id': 2, 'name': 'Campaign 2', 'tags': ['marketing', 'promotional']},
+        {'id': 3, 'name': 'Campaign 3', 'tags': ['marketing', 'promotional']}
+    ]
+
+    return render_template('index.html', campaigns=campaigns)
+
+def get_campaign():
+    print('here')
