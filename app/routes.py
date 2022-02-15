3fbb316a70d5cd6ee7650ed8b8f94d210e4d7a76
jonathanlao <jonathan.lao@braze.com>
2022-02-14 20:15:10 -0500

    get analytics and details

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,54 @@
 from flask import render_template, request
 from app import app
+from datetime import datetime
 import requests
+
+URL = 'https://rest.iad-01.braze.com/'
+API_KEY = '63462b71-be35-4dc6-a15c-8b9374cc3ba2'
+HEADER = {"Authorization": "Bearer " + API_KEY}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    url = 'https://rest.iad-01.braze.com/'
     campaign_list_uri = '/campaigns/list'
-    API_KEY = 'c2b6c371-37f6-4d00-b925-c98c88e6d3e2'
-    headers = {"Authorization": "Bearer " + API_KEY}
     params = {
         'page': 0,
-        'include_archived': True,
+        'include_archived': False,
         'sort_direction': 'asc',
-        'last_edit.time[gt]': '1992-06-28T23:59:59-5:00'
+        'last_edit.time[gt]': '2021-02-14T23:59:59-5:00'
     }
-    res = requests.get(url + campaign_list_uri, headers=headers, params=params)
-    print(res.json())
+    res = requests.get(URL + campaign_list_uri, headers=HEADER, params=params)
+    campaigns = res.json()['campaigns']
 
-    campaigns = [
-        {'id': 1, 'name': 'Campaign 1', 'tags': ['marketing', 'promotional']},
-        {'id': 2, 'name': 'Campaign 2', 'tags': ['marketing', 'promotional']},
-        {'id': 3, 'name': 'Campaign 3', 'tags': ['marketing', 'promotional']}
-    ]
+    # Mock Data
+    # [
+    #     {'id': 1, 'name': 'Campaign 1', 'tags': ['marketing', 'promotional']},
+    #     {'id': 2, 'name': 'Campaign 2', 'tags': ['marketing', 'promotional']},
+    #     {'id': 3, 'name': 'Campaign 3', 'tags': ['marketing', 'promotional']}
+    # ]
 
     return render_template('index.html', campaigns=campaigns)
 
 @app.route('/campaign', methods=['GET'])
 def get_campaign():
     id = request.args.get('id')
-    print(id)
-    return '22'
+    campaign_analytics_uri = '/campaigns/data_series'
+    params = {
+        'campaign_id': id,
+        'length': 2,
+        'ending_at': datetime.now().isoformat()
+    }
+    res = requests.get(URL + campaign_analytics_uri, headers=HEADER, params=params)
+    analytics = res.json()
+    # print(analytics)
+
+
+    campaign_details_uri = '/campaigns/details'
+    params = {
+        'campaign_id': id,
+    }
+    res = requests.get(URL + campaign_details_uri, headers=HEADER, params=params)
+    details = res.json()
+    # print(details)
+
+    return render_template('index.html', details=details, analytics=analytics)
